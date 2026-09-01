1c737ffc35e4c38b3760cb57438afde338a59484
Hugo Passe <hugo.passe@protonmail.com>
2026-09-01 15:49:32 +0200

    Some cleaning

--- flint-extras/src/nmod_mat_struct/test/t-toeplitz-like.c
+++ flint-extras/src/nmod_mat_struct/test/t-toeplitz-like.c
@@ -1,7 +1,6 @@
 #include <flint/flint.h>
 #include <flint/nmod_mat.h>
 #include <flint/nmod_vec.h>
-#include <stdio.h>
 #include <flint/test_helpers.h>
 #include "nmod_mat_struct.h"
 
@@ -15,44 +14,27 @@ TEST_FUNCTION_START(nmod_mat_toeplitz_like, state)
         long k = n_randtest_bits(state,6);
         ulong N = n_randtest_prime(state, 1);
     
-        //long m = 15;
-        //long n = 10;
-        //long k = 2;
-        //long N = 101;
-
         nmod_toeplitz_like_t mat;
         nmod_toeplitz_like_randtest(mat, m, n, k, N, state);
         
-        //nmod_mat_print_pretty(mat->G);
-        //nmod_mat_print_pretty(mat->H);
-
         nmod_mat_t dense_mat;
         nmod_mat_init(dense_mat, m, n, N);
         nmod_toeplitz_like_dense(mat, dense_mat);
         
-        //flint_printf("Dense representation:\n");
-        //nmod_mat_print(dense_mat);
-
-        //flint_printf("Displaced dense matrix:\n");
         nmod_mat_t disp_mat;
         nmod_mat_init(disp_mat, m, n, N);
         nmod_mat_displacement_operator(dense_mat, disp_mat);
-        //nmod_mat_print(disp_mat);
 
         nmod_mat_t GH, Ht;
         nmod_mat_init(Ht, k , n , N);
         nmod_mat_transpose(Ht, mat->H);
         nmod_mat_init(GH, m , n , N);
         nmod_mat_mul(GH, mat->G, Ht);
-        //nmod_mat_print(GH);
-
+        
         int result = nmod_mat_equal(GH, disp_mat);
-
         if (!result) TEST_FUNCTION_FAIL("Structured result and dense result do not coincide\n");
 
         nmod_mat_clear(dense_mat);
-
-        // Displacement checks
         nmod_mat_clear(disp_mat);
         nmod_mat_clear(GH);
         nmod_mat_clear(Ht);
